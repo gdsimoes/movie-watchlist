276fce1459e58fa278e72ffbf25dfba4cbd0f165
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-08-07 14:20:08 -0400

    Add api constants file

---
+++ scripts/api.js
@@ -0,0 +1,7 @@
+// API variables
+
+// Ideally, I should hide this API key, but since I am only using
+// browser JavaScript for this project, the key would be exposed anyway.
+export const apikey = "4efc850f";
+
+export const baseUrl = "https://www.omdbapi.com/";
