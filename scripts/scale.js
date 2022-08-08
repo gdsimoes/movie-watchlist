878ff672dfc7b5d0cbb5e0e33bcac45a74477ae1
Guilherme Dias Simoes <gds.simoes@gmail.com>
2022-08-08 18:30:02 -0400

    Add scaling functionality

---
+++ scripts/scale.js
@@ -0,0 +1,34 @@
+// Scale design according to screen size
+function scalar() {
+    const designWidth = 550;
+    const designHeight = 779;
+    const ratio = designWidth / designHeight;
+
+    const container = document.querySelector(".container");
+    const body = document.querySelector("body");
+
+    // Make the height equal to the screen height or the width
+    // equal to the width. Whatever can be done without changing
+    // the ratio of the original design.
+    // console.log(window.innerHeight, window.innerWidth * ratio);
+    if (window.innerWidth / window.innerHeight < ratio) {
+        const scalar = window.innerWidth / designWidth;
+        container.style.transform = `scale(${scalar})`;
+        // body.style.height = `${ratio * designHeight}px`;
+        console.log("HI");
+    } else {
+        // For bigger screens it's best to leave some room on the sides
+        // const ratioHeight =
+        //     (window.innerHeight / designHeight) * bufferConstant;
+        // const ratioWidth = (window.innerWidth / designWidth) * bufferConstant;
+        // const ratio = Math.min(ratioHeight, ratioWidth);
+        // container.style.transform = `scale(${ratio})`;
+        // body.style.height = `${ratio * designHeight}px`;
+        const scalar = window.innerHeight / designHeight;
+        container.style.transform = `scale(${scalar})`;
+        console.log("BYE");
+    }
+}
+
+window.addEventListener("resize", scalar);
+scalar();
